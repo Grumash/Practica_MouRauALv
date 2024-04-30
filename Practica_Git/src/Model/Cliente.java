692ecc74f80fd7f09681897e51f4637ddfee4832
Grumash <162335328+Grumash@users.noreply.github.com>
2024-04-30 17:09:55 +0200

    Clase Cliente Creada

--- Practica_Git/src/Model/Cliente.java
+++ Practica_Git/src/Model/Cliente.java
@@ -2,4 +2,40 @@ package Model;
 
 public class Cliente {
 
+	String Nombre;
+	String Apellido;
+	int Edad;
+
+    public Cliente(String Nombre, String Apellido, int Edad) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Edad = Edad;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+	
+	
+	
 }
