7a9aa2f91cc01ff98d7ddbb285d0dd884efcfdd9
velociti642 <131817459+velociti642@users.noreply.github.com>
2024-04-30 17:21:11 +0200

    Clase cuenta creada

--- Practica_Git/src/Model/Cuenta.java
+++ Practica_Git/src/Model/Cuenta.java
@@ -2,4 +2,15 @@ package Model;
 
 public class Cuenta {
 
+	private String IBAN;
+	private ArrayList<Cliente> cliente;
+	private double cantidad;
+	private boolena estado;
+	
+	public Cuenta() {
+		this.IBAN = IBAN;
+		this.cliente = new ArratList<>();
+		this.cantidad = cantidad;
+		this.estado = true;
+	}
 }
